27c912e9b4613a8be8fbce32f207fe00e402c24a
matorama <mathieufallows@gmail.com>
2015-02-09 10:18:24 -0800

    added player move and computer move code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,6 +22,13 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
+	var move 
+		if (move != null){
+			move = move;
+		}
+		else {
+			move = getInput();
+		}
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
@@ -29,6 +36,13 @@ function getPlayerMove(move) {
 }
 
 function getComputerMove(move) {
+	var move 
+		if (move != null){
+			move = move;
+		}
+		else {
+			move = randomPlay();
+		}
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
