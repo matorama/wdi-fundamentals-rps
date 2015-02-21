f028772f63249439571b5b4dba531ab076f71223
matorama <mathieufallows@gmail.com>
2015-02-20 18:40:30 -0800

    got the code to work but had to add <winner> as global variable

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -20,6 +20,7 @@ function randomPlay() {
 ////////////////////////////////////////////////
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
+var winner;
 
 function getPlayerMove(move) {
     var move 
@@ -52,7 +53,7 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -93,20 +94,22 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    var winner
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
-   while (playerWins <5 || computerWins < 5){   
+   while (playerWins <5 && computerWins < 5){   
         getWinner(getPlayerMove(), getComputerMove());
     
         if (winner == "player") {
         playerWins++;
+        console.log([playerWins, computerWins]);
         }
         else if (winner == "computer") {
         computerWins++;
+        console.log([playerWins, computerWins]);
         }
         else {
         playerWins++ && computerWins++;
+        console.log([playerWins, computerWins]);
         }
 
    }
